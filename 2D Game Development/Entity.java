8fd5184f1e0808d3b9ff8f0efb15f3d6f83cde08
Macarony <140412891+dev-nass@users.noreply.github.com>
2024-05-29 23:35:01 +0800

    Add files via upload

--- 2D Game Development/Entity.java
+++ 2D Game Development/Entity.java
@@ -7,6 +7,7 @@ public class Entity {
 	public int x, y;
 	public int speed;
 	
+	// since Entity class and Player class are both in the same package, Player class then now has an access to this Buffered Image
 	public BufferedImage frontIdle, backIdle, leftIdle, rightIdle, up1, up2, down1, down2, left1, left2, right1, right2; // use as a container for images
 	public String direction; // use to hold or indicate which direction the player is intended to move
 	
