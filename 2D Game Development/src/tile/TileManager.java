da59de1137e2d2aa534e103d47bee9387c1de53c
Macarony <140412891+dev-nass@users.noreply.github.com>
2024-05-31 06:27:05 +0800

    Add files via upload

--- 2D Game Development/src/tile/TileManager.java
+++ 2D Game Development/src/tile/TileManager.java
@@ -13,17 +13,17 @@ import main.GamePanel;
 public class TileManager {
 
 	GamePanel gp;
-	Tile[] tile;
-	int mapTileNum[] [];
+	public Tile[] tile;
+	public int mapTileNum[] [];
 	
 	public TileManager(GamePanel gp) {
 		this.gp = gp;
 		
 		tile = new Tile[10];
-		mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
+		mapTileNum = new int[gp.maxWorldCol][gp.maxWorldRow];
 		
 		getTileImage();
-		loadMap("/Maps/TilesPattern.txt");
+		loadMap("/Maps/world01.txt");
 	}
 	
 	public void getTileImage() {
@@ -31,16 +31,25 @@ public class TileManager {
 		try {
 			
 			tile[0] = new Tile();
-			tile[0].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/grassv2.png"));
+			tile[0].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/grass.png"));
 			
 			tile[1] = new Tile();
-			tile[1].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/grassv21.png"));
+			tile[1].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/wall.png"));
+			tile[1].collision = true;
 			
 			tile[2] = new Tile();
-			tile[2].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/wall.png"));
+			tile[2].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/water.png"));
+			tile[2].collision = true;
 			
 			tile[3] = new Tile();
-			tile[3].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/water.png"));
+			tile[3].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/earth.png"));
+			
+			tile[4] = new Tile();
+			tile[4].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/tree.png"));
+			tile[4].collision = true;
+			
+			tile[5] = new Tile();
+			tile[5].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/sand.png"));
 			
 			
 		} catch(IOException e) {
@@ -61,12 +70,12 @@ public class TileManager {
 			int col = 0;
 			int row = 0;
 			
-			while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
+			while (col < gp.maxWorldCol && row < gp.maxWorldRow) {
 				
 				// read the first line within the text file, and put it inside the String variable
 				String line = br.readLine();
 				
-				while (col < gp.maxScreenCol) {
+				while (col < gp.maxWorldCol) {
 					
 					// this then stores each numbers that's contain within the line variable, the (0, 1, 0) in accordance to the text file, because
 					// it automatically splits them by spaces. So the retrieval of the letter/numbers becomes individual
@@ -81,7 +90,7 @@ public class TileManager {
 					
 				}
 				// if the COL variables becomes equals to 16, which is the value of the maxScreenCol
-				if (col == gp.maxScreenCol) {
+				if (col == gp.maxWorldCol) {
 					// COL is reset again on the 0 value, meaning, it will be use again as 0 or first index of the mapTileNum array
 					col = 0;
 					// this code is added ensuring that the mapTileNum will stores the numbers on the next ROW
@@ -99,32 +108,50 @@ public class TileManager {
 	// Responsible for rendering the set map pattern
 	public void draw(Graphics2D g2) {
 		
-		int col = 0;
-		int row = 0;
-		int x = 0;
-		int y = 0;
+		// use as arguments for mapTileNum or printing what tile on that array index
+		int worldCol = 0;
+		int worldRow = 0;
+		
 		
 		// So basically, this WHILE loop fills the tiles horizontally (-) first:
-		while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
+		while (worldCol < gp.maxWorldCol && worldRow < gp.maxWorldRow) {
 			
-			// the [col][row] arguments are just there to retrieve what value of number are there in that index of the 2D array named mapTileNum
-			// so "what number does the mapTileNum stores in the of index [0] [0], that's 0 base on the txt file --
-			int tileNum = mapTileNum[col][row];
+			// the [worldCol][worldRow] arguments are just there to retrieve what value of number are there in that index of the 2D array named mapTileNum
+			// so "what number does the mapTileNum stores in the of index [0] [0], that's 0 base on the text file --
+			int tileNum = mapTileNum[worldCol][worldRow];
 			// -- this 0 is then passed on the tileNum variable. Then tileNum becomes an argument to access what pre-defined image that are there in tile[0] -- that's graasv2
-			// arguments as follows: image to be use, x and y coordinates, width, height, idk.
-			g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
 			
-			col++;
-			x = x + gp.tileSize;
+			// worldX and worldY are use for tile position too within the map. noticeable by the comment I don't 100% know the functions of these variables
+			int worldX = worldCol * gp.tileSize; // 0 * 48 = 0
+			int worldY = worldRow * gp.tileSize; // 0 * 48 = 0
+			
+			// screnX and Y are use as coordinates for drawing tiles position within the whole map
+			// whole world tilePosition = tile position - player's position within the text file / whole Map + ensures that the player on the center of the screen a.k.a Camera
+			// this line of code return NEGATIVE value, pass its value on screenX and screenY, ensuring that the loop prints the necessary tiles away from the user position 
+			int screenX = worldX - gp.player.worldX + gp.player.screenX;
+			int screenY = worldY - gp.player.worldY + gp.player.screenY;
+			
+			// this does, whenever the centered player walks along, the program only renders or prints those tiles that within its vicinity, see video#5
+			if (worldX + gp.tileSize > gp.player.worldX - gp.player.screenX &&
+				worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
+				worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
+				worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
+				// arguments as follows: image to be use, x and y coordinates, width, height, idk.
+				g2.drawImage(tile[tileNum].image, screenX, screenY, gp.tileSize, gp.tileSize, null);
+			}
+			
+			
+			worldCol++;
+		
 		
 	   // then as the horizontal line is filled indicating in this if and else statement that 
-	   // if COL becomes = 16, equal to screen width which is 16 too.
+	   // if worldCol becomes = 16, equal to screen width which is 16 too.
 	   // the Y variable increases causing the placement of the tile to go vertically ( | ) for once
-			if(col == gp.maxScreenCol) {
-				col = 0;
-				x = 0;
-				row++;
-				y = y + gp.tileSize;
+			if(worldCol == gp.maxWorldCol) {
+				worldCol = 0;
+			
+				worldRow++;
+				
 			}
 		}
 		
