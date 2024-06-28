f11226088598a714655e0a7efebf46ffe6009962
Macarony <140412891+dev-nass@users.noreply.github.com>
2024-06-28 15:33:09 +0800

    Add files via upload

---
+++ Exercises/Finals/containsWhiteSpace.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+// JUNE 28, 2024
+// Check if the user input any space within the text
+
+public class containsWhiteSpace  {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+    	
+    	String sUserName = "";
+    	do {
+    		System.out.print("Enter your name: ");
+            sUserName = scanner.nextLine();
+            methodWhiteSpaceChecker(sUserName);
+    	} while(methodWhiteSpaceChecker(sUserName));
+        
+    	System.out.println("Hello, "+sUserName);
+
+    }
+
+    static boolean methodWhiteSpaceChecker(String sTobeCheck) {
+
+        for (char a : sTobeCheck.toCharArray()) {
+            if(Character.isSpace(a)) {
+               return true;
+            }
+        }
+        return false;
+    }
+
+}
